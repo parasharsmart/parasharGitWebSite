f19a3e993a40d19dc74fb1717a3bf84b48a8b2f3
parasharsmart <parasharsmart@gmail.com>
2014-05-08 15:10:51 +0530

    First Commit

---
+++ ParasharGitWebsite/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ParasharGitWebsite.Startup))]
+namespace ParasharGitWebsite
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
